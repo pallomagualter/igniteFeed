87df2e11d6ef6d7a428b2b406bea473deae545bf
pallomagualter <pallomakcgualter@gmail.com>
2024-08-26 20:06:50 -0300

    feat: create post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from './Post.module.css';
+
+
+export function Post() {
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/pallomagualter.png"  
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Palloma Gualter</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='26 de Agosto de 2024 às 19:36h' dateTime="2024-08-26">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galera!</p>
+                <p>teste</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt repudiandae veritatis quae minima itaque. Suscipit odio ratione vero repudiandae quasi! Similique possimus consequatur maiores blanditiis totam esse. Cumque, ullam magni.</p>
+                <p>😝 <a href="">https://github.com/pallomagualter</a></p>
+                <p>
+                    <a href="">#Rocketseat</a>
+                    <a href=""> #React #boracodar</a>
+                </p>
+            </div>
+        </article>
+    );
+}
